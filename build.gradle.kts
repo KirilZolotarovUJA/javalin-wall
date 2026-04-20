8a97f3a0a6641ddc88267fba4d4e66f3df8c2e1d
Kiril Zolotarov <kz000012@red.ujaen.es>
2026-04-21 01:18:35 +0200

    Hello, world!

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    kotlin("jvm") version "2.3.10"
+}
+
+group = "es.ujaen"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {}
+
+kotlin {
+    jvmToolchain(21)
+}
---
+++ src/main/kotlin/es/ujaen/Main.kt
@@ -0,0 +1,5 @@
+package es.ujaen
+
+fun main() {
+    println("Hello, world")
+}
